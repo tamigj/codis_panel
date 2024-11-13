8cfd99e742106e066454030c0e474bc6e03334ce
Tami Gjorgjieva <tami@sh03-ln02.stanford.edu>
2024-11-12 23:09:15 -0800

    further cleanup

---
+++ analyses/distance/4_cleanup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#SBATCH --job-name=cleanup_distance
+#SBATCH --time=01:00:00
+#SBATCH --mem=1G
+#SBATCH --output=cleanup_distance.log
+#SBATCH --error=cleanup_distance.err
+#SBATCH -p hns,owners,normal
+#SBATCH -c 1
+
+source /scratch/groups/noahr/tami/codis_panel/config.sh
+
+#---------------------------#
+# EDIT FOR EACH EXPERIMENT  #
+#---------------------------#
+experiment='distance'
+n_snps_per_str=$SELECTED_PANEL_SIZES
+n_snp_reps=10
+n_ind_reps=10
+
+###############################################################
+
+for distance_filter in $DISTANCE_FILTERS;
+do
+
+  for n_snps in $n_snps_per_str;
+  do
+
+    for n_snp_rep in $(seq 1 $n_snp_reps);
+    do
+
+      for n_ind_rep in $(seq 1 $n_ind_reps);
+      do
+
+        for fraction in $FRACTIONS;
+        do
+
+          snplist_id=${experiment}_${distance_filter}_${n_snps}_${n_snp_rep}
+          n_run=${snplist_id}_${n_ind_rep}
+
+          $DIR_CODE/cleanup_intermediate_files.sh \
+          $snplist_id $n_run $fraction
+
+        done
+      done
+    done
+  done
+done
---
+++ analyses/payseur_d/4_cleanup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#SBATCH --job-name=cleanup_payseur_d
+#SBATCH --time=01:00:00
+#SBATCH --mem=1G
+#SBATCH --output=cleanup_payseur_d.log
+#SBATCH --error=cleanup_payseur_d.err
+#SBATCH -p hns,owners,normal
+#SBATCH -c 1
+
+source /scratch/groups/noahr/tami/codis_panel/config.sh
+
+#---------------------------#
+# EDIT FOR EACH EXPERIMENT  #
+#---------------------------#
+experiment='payseur_d'
+n_snps_per_str=$SELECTED_PANEL_SIZES
+n_snp_reps=10
+n_ind_reps=10
+
+###############################################################
+
+for payseur_d_filter in $PAYSEUR_D_FILTERS;
+do
+
+  for n_snps in $n_snps_per_str;
+  do
+
+    for n_snp_rep in $(seq 1 $n_snp_reps);
+    do
+
+      for n_ind_rep in $(seq 1 $n_ind_reps);
+      do
+
+        for fraction in $FRACTIONS;
+        do
+
+          snplist_id=${experiment}_${payseur_d_filter}_${n_snps}_${n_snp_rep}
+          n_run=${snplist_id}_${n_ind_rep}
+
+          $DIR_CODE/cleanup_intermediate_files.sh \
+          $snplist_id $n_run $fraction
+
+        done
+      done
+    done
+  done
+done
