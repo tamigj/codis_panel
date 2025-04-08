58f0b4371776d12965bc75d04eee651467dd6243
Tami Gjorgjieva <tami@sh03-ln02.stanford.edu>
2025-04-08 14:25:20 -0700

    Keep only 0.75 fraction

--- analyses/data_processing/2_prepare_partitions.sh
+++ analyses/data_processing/2_prepare_partitions.sh
@@ -5,7 +5,7 @@ source /scratch/groups/noahr/tami/codis_panel/config.sh
 ml R/4.2
 
 for n_ind_rep in {1..100}; do
-  for fraction_refs in 0.25 0.5 0.75; do
+  for fraction_refs in 0.75; do
 
     out_dir="$DIR_DATA_PARTITIONS/${fraction_refs}/"
     Rscript 2a_make_ref_and_test_ids.R "$fraction_refs" "$n_ind_rep" "$out_dir"
